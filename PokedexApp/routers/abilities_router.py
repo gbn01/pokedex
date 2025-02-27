fcdf511780e11b908b4d9ecdef89208d46dca13b
Gabriel Saldanha <gabrielsaldanha@Gabriels-MacBook-Pro.local>
2025-02-27 14:39:55 -0300

    Changed type field in abilities to a reference to the type document

--- PokedexApp/routers/abilities_router.py
+++ PokedexApp/routers/abilities_router.py
@@ -1,11 +1,11 @@
 from bson import ObjectId
 from ..config.database import get_database
-from ..models.models import Ability
+from ..models.models import Ability, Type
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from pydantic.fields import Field
 from motor.motor_asyncio import AsyncIOMotorDatabase
-
+import uuid
 router = APIRouter()
 
 class AbilityRequest(BaseModel):
@@ -30,33 +30,71 @@ class AbilityRequest(BaseModel):
                     "pp": 15
                 }
             ]
-        }
+        },
     }
 
-@router.get("", response_model=list[Ability])
+class AbilityResponse(Ability):
+    name: str
+    description: str
+    power: int
+    accuracy: int
+    type: Type
+    category: str
+    pp: int
+
+@router.get("", response_model=list[AbilityResponse])
 async def get_abilities(db: AsyncIOMotorDatabase = Depends(get_database)):
-    abilities = await db.abilities.find({}).to_list(length=100)
+    abilities = await db.abilities.aggregate([{
+        "$lookup": {
+            "from": "types",
+            "localField": "type",
+            "foreignField": "_id",
+            "as": "type"
+        }
+    },
+    {
+        "$unwind": "$type"
+    }]).to_list(length=100)
+
     return abilities
 
 @router.post("", status_code=200, response_model=Ability)
 async def create_ability(ability: AbilityRequest, db: AsyncIOMotorDatabase = Depends(get_database)):
-    result = await db.abilities.insert_one(ability.model_dump())
-    print(result)
+    abilityDoc = ability.model_dump()
+    abilityDoc["_id"] = str(uuid.uuid4())
+    result = await db.abilities.insert_one(abilityDoc)
     return await db.abilities.find_one({"_id": result.inserted_id})
 
-@router.get("/{ability_id}", response_model=Ability)
+@router.get("/{ability_id}", response_model=AbilityResponse)
 async def get_ability(ability_id: str, db: AsyncIOMotorDatabase = Depends(get_database)):
-    ability = await db.abilities.find_one({"_id": ObjectId(ability_id)})
-    return ability
+    ability = await db.abilities.aggregate([{
+        "$match": {"_id": ability_id}
+    },
+    {
+        "$lookup": {
+            "from": "types",
+            "localField": "type",
+            "foreignField": "_id",
+            "as": "type"
+        }
+    },
+    {
+        "$unwind": "$type"
+    }]).to_list(length=1)
+    print(ability)
+    if len(ability) == 0:
+        raise HTTPException(status_code=404, detail="Ability not found")
+    return ability[0]
 
 @router.put("/{ability_id}", response_model=Ability)
 async def update_ability(ability_id: str, ability: AbilityRequest, db: AsyncIOMotorDatabase = Depends(get_database)):
-    result = await db.abilities.update_one({"_id": ObjectId(ability_id)}, {"$set": ability.model_dump()})
-    print(result)
-    return await db.abilities.find_one({"_id": ObjectId(ability_id)})
+    result = await db.abilities.update_one({"_id": ability_id}, {"$set": ability.model_dump()})
+    if result.modified_count == 0:
+        raise HTTPException(status_code=404, detail="Ability not found")
+    return await db.abilities.find_one({"_id": ability_id})
 
 @router.delete("/{ability_id}", status_code=204)
 async def delete_ability(ability_id: str, db: AsyncIOMotorDatabase = Depends(get_database)):
-    result = await db.abilities.delete_one({"_id": ObjectId(ability_id)})
+    result = await db.abilities.delete_one({"_id": ability_id})
     if result.deleted_count == 0:
         raise HTTPException(status_code=404, detail="Ability not found")
--- PokedexApp/routers/types_router.py
+++ PokedexApp/routers/types_router.py
@@ -1,3 +1,4 @@
+import uuid
 from bson import ObjectId
 from ..config.database import get_database
 from ..models.models import Type
@@ -11,31 +12,42 @@ router = APIRouter()
 class TypeRequest(BaseModel):
     name: str = Field(..., description="The name of the type", max_length=20, min_length=3)
 
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {"name": "Fire"},
+            ]
+        }
+    }
+
 @router.get("", response_model=list[Type])
 async def get_types(db: AsyncIOMotorDatabase = Depends(get_database)):
     types = await db.types.find({}).to_list(length=100)
     return types
 
-@router.post("", status_code=201)
+@router.post("", status_code=200, response_model=Type)
 async def create_type(type: TypeRequest, db: AsyncIOMotorDatabase = Depends(get_database)):
-    result = await db.types.insert_one(type.model_dump())
+    typeDoc = type.model_dump()
+    typeDoc["_id"] = str(uuid.uuid4())
+    result = await db.types.insert_one(typeDoc)
+    return await db.types.find_one({"_id": result.inserted_id})
 
 @router.get("/{type_id}", response_model=Type)
 async def get_type(type_id: str, db: AsyncIOMotorDatabase = Depends(get_database)):
-    type = await db.types.find_one({"_id": ObjectId(type_id)})
+    type = await db.types.find_one({"_id": type_id})
     if not type:
         raise HTTPException(status_code=404, detail="Type not found")
     return type
 
 @router.put("/{type_id}", response_model=Type)
 async def update_type(type_id: str, type: TypeRequest, db: AsyncIOMotorDatabase = Depends(get_database)):
-    result = await db.types.update_one({"_id": ObjectId(type_id)}, {"$set": type.model_dump()})
+    result = await db.types.update_one({"_id": type_id}, {"$set": type.model_dump()})
     if result.modified_count == 0:
         raise HTTPException(status_code=404, detail="Type not found")
-    return result.upserted_id
+    return await db.types.find_one({"_id": type_id})
 
 @router.delete("/{type_id}", status_code=204)
 async def delete_type(type_id: str, db: AsyncIOMotorDatabase = Depends(get_database)):
-    result = await db.types.delete_one({"_id": ObjectId(type_id)})
+    result = await db.types.delete_one({"_id": type_id})
     if result.deleted_count == 0:
         raise HTTPException(status_code=404, detail="Type not found")
